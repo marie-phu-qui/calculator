8681bce7d5954fe37f3b247a0f6f80c8a061b594
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-06-14 15:28:43 +1200

    shapped buttons

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
 var entries = [];
 var total = 0;
+var temp = '';
 
+  // Got a number, add to temp
+  // Push number to screen
+
+
+  // Got some symbol other than equals, add temp to our entries
+  // then add our current symbol and clear temp
