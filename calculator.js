3cbc0191650d9d24f137afe3b5665f2ef46dab3f
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-06-19 22:06:36 +1200

    displayNum function created, numbers displayed on screen

--- calculator.js
+++ calculator.js
@@ -1,10 +1,5 @@
-var entries = [];
-var total = 0;
-var temp = '';
 
-  // Got a number, add to temp
-  // Push number to screen
-
-
-  // Got some symbol other than equals, add temp to our entries
-  // then add our current symbol and clear temp
+function displayNum(i){ 
+    document.getElementById("screen").value += i ;
+    }
+    
