5c323266aeb3173c1edba557b99c0101f6b479ba
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-06-20 13:23:52 +1200

    clear screen when operator clicked

--- calculator.js
+++ calculator.js
@@ -1,18 +1,46 @@
 let screenDisplay = [];
-
+let numOne = 0;
+let numTwo = 0;
 
 function displayNum(i){ 
     screenDisplay = document.getElementById("screen").value;
-    if (screenDisplay.length>16){
-        sound();
+    if(screenDisplay.length>16){
+        stop();
     }
-    else {
+    else{
         document.getElementById("screen").value += i ;
     }
-    }
+    };
+
+function clearScreen(){
+    let numOne =document.getElementById("screen").value;
+    let screenDisplay = [];
+    document.getElementById("screen").value = screenDisplay;
+
+}
+
+function add(){
+    numOne + numOne;
+}
+
+function minus(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+
+function modulo(){
+
+}
     
-// honk can't go further sound
-function sound(){
+// can't go further sound
+function stop(){
     screenDisplay = document.getElementById("screen");
     console.log("can't make this number longer");
     screenDisplay.style.backgroundColor="grey";
